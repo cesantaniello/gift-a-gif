99af282b7dbde47f51133434e3bfc013aa65c373
Administrator <admin@example.com>
2021-12-01 17:40:50 +0100

    12. Agrega animaciones con Animate.css

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -8,7 +8,7 @@ export const GifExpertApp = () => {
 
     return (
         <>
-            <h2>GifExpertApp</h2>
+            <h2>Gift-a-gif</h2>
             <AddCategory setCategories={setCategories} />
             <hr />
 
