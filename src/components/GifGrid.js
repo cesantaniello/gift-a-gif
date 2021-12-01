79b7fad31e680e8583a2d99e7cbf570f4e3bea5e
Administrator <admin@example.com>
2021-12-01 11:47:08 +0100

    9. Agrega helpers en getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,15 @@
 import React, {useState,useEffect} from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({category}) => {
 
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=batman&limit=10&api_key=AnCjC7Tgzj8tJO9Jj91ImRnw3mdlMsqp'
-        const resp = await fetch(url);
-        const {data} = await resp.json();
-
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-        console.log(gifs);
-        setImages(gifs);
-    }
-
+        getGifs(category)
+            .then(setImages);
+    }, [category]);
 
     return (
         <>
