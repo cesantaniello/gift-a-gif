46824665c14cf3a65bcd4e33c9ab546dde41d667
Administrator <admin@example.com>
2022-01-03 19:48:22 +0100

    Implementa PropTypes y actualiza Snapshot

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+    const title = 'Titulo de prueba';
+    const url = 'https://localhost/imagen.jpg';
+    
     test('Debe de mostrarse correctamente', () => {
-        const wrapper = shallow(<GifGridItem  />)
+        const wrapper = shallow( <GifGridItem  title={ title } url={ url } /> )
         expect(wrapper).toMatchSnapshot();
     })
 })
