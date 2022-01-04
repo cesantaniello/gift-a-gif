372672d7c9614343fb76c4e9bdb5d405e8f6ee22
Administrator <admin@example.com>
2022-01-04 14:09:02 +0100

    Simula un submit del formulario

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,6 +12,7 @@ export const AddCategory = ({setCategories}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('handleSubmit llamado', inputValue);
 
         if(inputValue.trim().length > 2){
             setCategories(cats => [inputValue, ...cats]);
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+
 import {shallow} from "enzyme";
 import {AddCategory} from "../../components/AddCategory";
 
 describe('Pruebas en <AddCategory />', () => {
-    const setCategories = () => {};
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    const setCategories = jest.fn();
+    let wrapper;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    });
 
     test('Debe mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
@@ -17,5 +24,10 @@ describe('Pruebas en <AddCategory />', () => {
         input.simulate('change', {target: {value}});
 
         expect(wrapper.find('p').text().trim()).toBe(value);
+    })
+
+    test('No debe de postear la información', () => {
+        wrapper.find('form').simulate('submit', {preventDefault() {}});
+        expect(setCategories).not.toHaveBeenCalled();
     })
 })
