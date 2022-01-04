90cb980eb9f48f7846858e297a4f4d85e84dae83
Administrator <admin@example.com>
2022-01-04 12:58:55 +0100

    Prueba en AddCategory

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from "enzyme";
+import {AddCategory} from "../../components/AddCategory";
+
+describe('Pruebas en <AddCategory />', () => {
+    const setCategories = () => {};
+
+    test('Debe mostrarse correctamente', () => {
+        const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+        expect(wrapper).toMatchSnapshot();
+    })
+})
