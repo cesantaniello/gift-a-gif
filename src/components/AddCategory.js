1ddb7c17b9189f144d5c2cd8efdfea2deca901f3
Administrator <admin@example.com>
2022-01-04 13:41:34 +0100

    Simula cambios en input

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -7,6 +7,7 @@ export const AddCategory = ({setCategories}) => {
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
+        console.log('handleInputChange llamado');
     }
 
     const handleSubmit = (e) => {
@@ -22,6 +23,7 @@ export const AddCategory = ({setCategories}) => {
     return (
         <>
         <form onSubmit={handleSubmit}>
+            <p>{inputValue}</p>
             <input 
                 type="text" 
                 value={inputValue} 
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,9 +4,18 @@ import {AddCategory} from "../../components/AddCategory";
 
 describe('Pruebas en <AddCategory />', () => {
     const setCategories = () => {};
+    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
     test('Debe mostrarse correctamente', () => {
-        const wrapper = shallow(<AddCategory setCategories={setCategories} />);
         expect(wrapper).toMatchSnapshot();
+    })
+
+    test('Debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', {target: {value}});
+
+        expect(wrapper.find('p').text().trim()).toBe(value);
     })
 })
